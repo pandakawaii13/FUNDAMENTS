bfee0a3012a563d358e67a16f0ee518e3773b74f
pandakawaii13 <47011434+pandakawaii13@users.noreply.github.com>
2019-01-29 21:10:46 -0500

    Add files via upload

---
+++ DESAFIO 1-4 class 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFIO_1_4_class_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ingrese w,t,c");
+            // INGRESAR DATOS
+            double w = double.Parse(Console.ReadLine());
+            double t = double.Parse(Console.ReadLine());
+            double cGrados = double.Parse(Console.ReadLine());
+
+            // CONVERSIÓN GRADOS - RADIANES
+            double c = cGrados * (Math.PI / 180.0);
+            // HALLAR Z
+            double z = Math.Sin(c) * t;
+            // HALLAR Y
+            double y = Math.Sqrt((t * t) - (z * z));
+            double xy = (w * w) - (z * z);
+            double x = Math.Sqrt(xy) - y;
+
+            Console.WriteLine("x =" + x);
+
+
+        }
+    }
+
+}
+
+
