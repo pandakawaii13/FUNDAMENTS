e0c56f769f299ff6e8e717ab9e6f6df787d32416
pandakawaii13 <47011434+pandakawaii13@users.noreply.github.com>
2019-01-29 21:10:27 -0500

    Add files via upload

---
+++ DESAFIO 1-3 class 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFIO_1_3_class_2
+{
+     public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ingrese w,d,x");
+            // INGRESAR DATOS
+            double w = double.Parse(Console.ReadLine());
+            double dGrados = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+
+            // CONVERSIÓN GRADOS - RADIANES
+            double d = dGrados * (Math.PI / 180.0);
+            // HALLAR Z
+            double z = Math.Sin(d) * w;
+            //HALLAR Y
+            double y = (z / Math.Tan(d)) - x;
+
+
+            Console.WriteLine("y =" + y);
+
+
+        }
+    }
+}
