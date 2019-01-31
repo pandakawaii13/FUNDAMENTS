63b38873312fd0fc92b359d98b7bc6498524eeaf
pandakawaii13 <47011434+pandakawaii13@users.noreply.github.com>
2019-01-31 10:59:25 -0500

    Add files via upload

---
+++ DESAFIO 3-1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFIO_3_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Por favor ingrese el número de SMMLV que gana");
+            double No = double.Parse(Console.ReadLine());
+            double SMMLV = No * 828116;
+
+            if (SMMLV < 1656232)
+            {
+                Console.WriteLine("Debe cancelar la tarifa A");
+            }
+            //4 SALARIOS SON 3312464
+            else if (1656232 <= SMMLV && SMMLV <= 3312463)
+            {
+                Console.WriteLine("Debe cancelar la tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Debe cancelar la tarifa C");
+            }
+
+        }
+    }
+}
---
+++ DESAFIO 3-2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFIO_3_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Por favor ingrese el número de SMMLV que gana");
+            double No = double.Parse(Console.ReadLine());
+            double SMMLV = No * 828116;
+
+            if (SMMLV < 1656232)
+            {
+                Console.WriteLine("Debe cancelar la tarifa A");
+                Console.WriteLine("Y debe cancelar $3200");
+            }
+            //5 SALARIOS SON 4140579
+            else if (1656232 <= SMMLV && SMMLV <= 4140579)
+            {
+                Console.WriteLine("Debe cancelar la tarifa B");
+                Console.WriteLine("Y debe cancelar $12700");
+            }
+            else
+            {
+                Console.WriteLine("Debe cancelar la tarifa C");
+                Console.WriteLine("Y debe cancelar $33500");
+            }
+
+        }
+    }
+}
